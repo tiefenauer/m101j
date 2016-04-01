509ae61db83e11d31839de44ef26ac3c1b8379da
Daniel Tiefenauer <git@tiefenauer.info>
2016-04-01 11:14:53 +0200

    adding courseware for week02

--- week01/code/src/main/java/info/tiefenauer/m101j/HelloWorldSparkFreemarkerStyle.java
+++ week01/code/src/main/java/info/tiefenauer/m101j/HelloWorldSparkFreemarkerStyle.java
@@ -2,13 +2,11 @@ package info.tiefenauer.mongodbcourse;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
-import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
